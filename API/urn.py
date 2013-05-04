45dc16859903cc72e5f10b6ee2c02d1a5226241f
Fraser Rackham <frackham@googlemail.com>
2013-05-04 07:56:36 -0700

    Initial setup.

---
+++ API/urn.py
@@ -0,0 +1,23 @@
+
+
+class School():
+	def __init__(self):
+		#Could be defined by multiple constructors...
+		pass
+	#properties...
+	#head, location, academic cohort snapshot, links.
+
+
+
+
+class SchoolDataSourceUri():
+	def __init__(self, baseuri, description, uriconstructor):
+		# uses <> as placeholder in the uriconstructor to show urn position.
+		# may need to generalise to non urn id numbers?
+		#e.g. ("www.raiseonline.org", "Raise website provides summary and interactive analyses...", 
+		#	   "www.raiseonline.org/demo?urnid=<>")
+		pass
+
+
+
+
