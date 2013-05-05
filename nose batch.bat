7ad6072a65e82b06dc664363f312ec86b81c9441
Fraser Rackham <frackham@googlemail.com>
2013-05-05 19:50:50 +0100

    Updated batch file to generate HTML and XML outputs using Coverage.

--- nose batch.bat
+++ nose batch.bat
@@ -1,2 +1,9 @@
-nosetests -vv --collect-only
+nosetests -V 
+nosetests --collect-only
+nosetests -vv 
+REM nosetests -v
+REM nosetests
+REM nosetests --with-coverage
+nosetests --with-coverage --cover-html --cover-xml
+REM nosetests --with-doctest
 PAUSE 
